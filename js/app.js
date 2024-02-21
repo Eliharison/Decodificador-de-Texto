58d797ddd992619d6ec4a437f85d4a06f1fb7215
Eliharison Gabriel <124097279+Eliharison@users.noreply.github.com>
2024-02-21 02:31:55 -0300

    Creating encrypt function
    Criei a função criptografia, e para isso precisei criar outras funções para melhor entendimento do código.
    
    Obs: a Section decodificador aumenta de tamanho para os lados o que precisa ser refatorado em próximos commits.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,61 @@
-const textarea = document.querySelector('.textarea textarea');
-const placeholder = document.querySelector('.placeholder');
+const textarea = document.querySelector(".textarea textarea");
+const placeholder = document.querySelector(".placeholder");
 
-textarea.addEventListener('input', function() {
-  placeholder.style.display = this.value ? 'none' : 'block';
+function criptografar() {
+  const valor = document.querySelector("#textoPrincipal").value;
+
+  if (valor) {
+    adicionar__estilo__filhos("mensagem__inexistente", "display: none;");
+
+    const caracter__separado = valor.split("");
+    let texto__criptografado = "";
+    caracter__separado.forEach((elemento) => {
+      texto__criptografado += cifra__cesar(elemento, 3);
+    });
+
+    adicionar__html("p", "resultado", texto__criptografado);
+  } else {
+    remover__filhos("resultado")
+    adicionar__estilo__filhos("mensagem__inexistente", "display: block;");
+  }
+}
+
+function adicionar__html(tagFilha, id, conteudo) {
+  let filho = document.createElement(tagFilha);
+  let pai = document.getElementById(id);
+  filho.textContent = conteudo;
+
+  pai.appendChild(filho);
+}
+
+function adicionar__estilo__filhos(id, estilo) {
+  let section = document.getElementById(id);
+  if (section) {
+    let filhos = section.children;
+    for (let i = 0; i < filhos.length; i++) {
+      filhos[i].style.cssText += estilo;
+    }
+  }
+}
+
+function remover__filhos(id) {
+  let section = document.getElementById(id);
+  while (section.firstChild) {
+    section.removeChild(section.firstChild);
+  }
+}
+
+function cifra__cesar(letra, deslocamento) {
+  if (letra.match(/[a-zA-Z]/)) {
+    let codigo__ascii = letra.charCodeAt(0);
+    let limite = codigo__ascii <= 90 ? 65 : 97;
+    return String.fromCharCode(
+      codigo__ascii - limite + (deslocamento % 26) + limite
+    );
+  }
+  return letra;
+}
+
+textarea.addEventListener("input", function () {
+  placeholder.style.display = this.value ? "none" : "block";
 });
